b555fca2c66061e2b22fa6844de9dc89f28b2c4d
a.harkevich <vipigrog11@gmail.com>
2018-05-02 23:38:30 +0300

    fixing

--- InvestmentPlatform/InvestmentPlatform/App_Start/IdentityConfig.cs
+++ InvestmentPlatform/InvestmentPlatform/App_Start/IdentityConfig.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
-using InvestmentPlatform.Models;
 using InvestmentPlatform.Domain.Models;
 
 namespace InvestmentPlatform
